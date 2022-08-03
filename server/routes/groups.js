0e1426580d8a6be47ea8d752862e15943e8a0faf
Alex Rodgers <skyewulff@outlook.com>
2022-08-03 16:08:50 -0400

    add GET route for getting a group by its id

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -14,5 +14,18 @@ groupRouter.get("/", (req, res, next) => {
     });
 });
 
+groupRouter.get("/:groupId", (req, res, next) => {
+    Group.findById(req.params.groupId, (err, group) => {
+        if(err) {
+            res.status(500).send("There was an error with your request");
+        } else {
+            if(!group) {
+                res.status(404).send("Group not found");
+            } else {
+                res.status(200).send(group);
+            }
+        }
+    });
+});
 
 module.exports = groupRouter;

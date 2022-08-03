ef74d6f9c56395f3be419e0189261c04dc985648
Alex Rodgers <skyewulff@outlook.com>
2022-08-03 16:56:19 -0400

    refactor groupRouter to use the param method before hitting group routes

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -3,6 +3,21 @@ const groupRouter = express.Router();
 
 const Group = require("../models/group");
 
+groupRouter.param("groupId", (req, res, next, groupId) => {
+    Group.findById(req.params.groupId, (err, group) => {
+        if(err) {
+            res.status(500).send("There was an error with your request");
+        } else {
+            if(!group) {
+                res.status(404).send("Group not found");
+            } else {
+                req.group = group;
+                next();
+            }
+        }
+    });
+});
+
 groupRouter.get("/", (req, res, next) => {
     Group.find({}, (err, groups) => {
         if(err) {
@@ -15,17 +30,8 @@ groupRouter.get("/", (req, res, next) => {
 });
 
 groupRouter.get("/:groupId", (req, res, next) => {
-    Group.findById(req.params.groupId, (err, group) => {
-        if(err) {
-            res.status(500).send("There was an error with your request");
-        } else {
-            if(!group) {
-                res.status(404).send("Group not found");
-            } else {
-                res.status(200).send(group);
-            }
-        }
-    });
+    res.status(200).send(req.group);
 });
+
 
 module.exports = groupRouter;

b733ef700e33811c4e0be4b770e5558702d8ef57
Alex Rodgers <skyewulff@outlook.com>
2022-08-02 22:03:10 -0400

    add decks property to Group model

--- server/models/group.js
+++ server/models/group.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const Group = new Schema({
     name: String,
-    members: [{type: Schema.Types.ObjectId, ref: "user"}]
+    members: [{type: Schema.Types.ObjectId, ref: "user"}],
+    decks: [{type: Schema.Types.ObjectId, ref: "deck"}]
 });
 
 module.exports = mongoose.model("group", Group);

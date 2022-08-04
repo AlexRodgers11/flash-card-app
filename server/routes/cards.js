26bc9662de6d9461c519c83d8aac666e83978689
Alex Rodgers <skyewulff@outlook.com>
2022-08-04 16:56:29 -0400

    add PUT route to edit a card

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -23,4 +23,15 @@ cardRouter.get("/:cardId", (req, res, next) => {
     res.status(200).send(req.card);
 });
 
+cardRouter.put("/:cardId", (req, res, next) => {
+    Card.findByIdAndUpdate(req.card._id, req.body, (err, card) => {
+        if(err) {
+            res.status(500).send("There was an error with your request");
+            throw err;
+        } else {
+            res.status(200).send(card);
+        }
+    });
+});
+
 module.exports = cardRouter;

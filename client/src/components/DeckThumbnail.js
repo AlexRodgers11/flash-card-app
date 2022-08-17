baa82d803231012612b17279aad4f0fae8a5862d
Alex Rodgers <skyewulff@outlook.com>
2022-08-17 15:04:47 -0400

    create DeckThumbnail component

---
+++ client/src/components/DeckThumbnail.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { useEffect } from 'react';
+import axios from "axios";
+
+function DeckThumbnail(props) {
+    const [deckData, setDeckData] = useState({});
+
+    useEffect(() => {
+        const baseURL = 'http://localhost:8000';
+        axios.get(`${baseURL}/decks/${props.deckId}`)
+            .then((response) => setDeckData(response.data))
+            .catch((err) => console.log(err));
+    }, [props.deckId]);
+  
+    return (
+    <div>
+        <h1>{deckData.name}</h1>
+        <p>{deckData.creator}</p>
+        <p>{deckData.dateCreated}</p>
+        <p>{deckData.public}</p>
+    </div>
+  )
+}
+
+DeckThumbnail.propTypes = {
+    deckId: PropTypes.string
+}
+
+export default DeckThumbnail

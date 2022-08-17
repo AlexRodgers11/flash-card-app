3b5f02816032bb649cdc3812bff446d8ba69e328
Alex Rodgers <skyewulff@outlook.com>
2022-08-17 12:13:20 -0400

    create DeckList component and connect it to decksSlice of store and add action dispatchers upon render

---
+++ client/src/components/DeckList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDecksOfCategory, fetchDecksOfGroup, fetchDecksOfUser } from '../reducers/decksSlice';
+
+export default function DeckList(props) {
+    const dispatch = useDispatch();
+    const deckIds = useSelector((state) => state.decks.deckIds);
+    const listType = useSelector((state) => state.decks.listType);
+    const listId = useSelector((state) => state.decks.listId);
+
+    useEffect(() => {
+        console.log("use effect running");
+        if(!listType && !listId) {
+            switch(props.listType) {
+                case "category":
+                    dispatch(fetchDecksOfCategory(props.listId));
+                    break;
+                case "group":
+                    dispatch(fetchDecksOfGroup(props.listId));
+                    break;
+                case "user":
+                    dispatch(fetchDecksOfUser(props.listId));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }, [deckIds, dispatch, listId, listType, props.listId, props.listType]);
+
+    return (
+        <div>
+            Test
+            {deckIds.map(deckId => <p>{deckId}</p>)}
+        </div>
+    )
+}

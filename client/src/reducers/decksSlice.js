cf9e57c28a960aee8d0bfad7025cbc51411f9857
Alex Rodgers <skyewulff@outlook.com>
2022-08-17 12:10:56 -0400

    restructure state in decksSlice, add reducer for getting a group's decks, and register all the fetch reducers as extraReducers

--- client/src/reducers/decksSlice.js
+++ client/src/reducers/decksSlice.js
@@ -4,22 +4,47 @@ import axios from "axios";
 const baseURL = 'http://localhost:8000';
 
 const initialState = {
-    decks: []
+    listType: "", //group/category/user
+    listId: "",//id of group/category/user that list of decks belongs to
+    deckIds: []
 };
 
 export const fetchDecksOfUser = createAsyncThunk("", async (userId) => {
+    console.log("in fetchDecksOfUser");
+    console.log(baseURL + "users/" + userId + "/decks");
     try {
-        const response = await axios.get(baseURL + "users/" + userId + "/decks");
-        return [...response.data];
+        const response = await axios.get(baseURL + "/users/" + userId + "/decks");
+        return {
+            listType: "user", 
+            listId: userId,
+            deckIds: [...response.data],
+        }
     } catch (err) {
-        return err.message;
+        return err;
     }
 });
 
 export const fetchDecksOfCategory = createAsyncThunk("decks/fetchDecksOfCategory", async (categoryId) => {
     try {
-        const response = await axios.get(baseURL + "/categories/" + categoryId);
-        return [...response.data];
+        const response = await axios.get(baseURL + "/categories/" + categoryId + "/decks");
+        return {
+            listType: "category",
+            listId: categoryId,
+            deckIds: [...response.data]
+        };
+    } catch (err) {
+        return err;
+    }
+});
+
+export const fetchDecksOfGroup = createAsyncThunk("decks/fetchDecksOfGroup", async (groupId) => {
+    try {
+        const response = await axios.get(baseURL + "/groups/" + groupId + "/decks");
+        return {
+            listType: "group",
+            listId: groupId,
+            deckIds: [...response.data]
+        };
     } catch (err) {
         return err;
     }
@@ -28,10 +53,26 @@ export const fetchDecksOfCategory = createAsyncThunk("decks/fetchDecksOfCategory
 export const decksSlice = createSlice({
     name: "decks",
     initialState,
-    reducers: {},
+    reducers: {
+        
+    },
     //for async requests
     extraReducers: (builder) => {
-
+        builder.addCase(fetchDecksOfUser.fulfilled, (state, action) => {
+            state.listType = action.payload.listType;
+            state.listId = action.payload.listId;
+            state.deckIds = action.payload.deckIds;
+        });
+        builder.addCase(fetchDecksOfCategory.fulfilled, (state, action) => {
+            state.listType = action.payload.listType;
+            state.listId = action.payload.listId;
+            state.deckIds = action.payload.deckIds;
+        });
+        builder.addCase(fetchDecksOfGroup.fulfilled, (state, action) => {
+            state.listType = action.payload.listType;
+            state.listId = action.payload.listId;
+            state.deckIds = action.payload.deckIds;
+        });
     }
 
 });

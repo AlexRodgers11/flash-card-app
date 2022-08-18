0fdb90fd1a1313cd9a7fe23f257ac8893879ab8a
Alex Rodgers <skyewulff@outlook.com>
2022-08-17 20:45:31 -0400

    set up state in Group component to track the groupSlice of store

--- client/src/components/Group.js
+++ client/src/components/Group.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router'
+import { fetchGroupData } from '../reducers/groupSlice';
 import DeckList from './DeckList'
+import GroupMemberList from './GroupMemberList';
 
 function Group() {
+    const dispatch = useDispatch();
     let { groupId } = useParams();
-  return (
-    <div>
-        <DeckList listType="group" listId={groupId} />
-    </div>
+    console.log(`paramsGroupId: ${groupId}`);
+    let storedGroupId = useSelector((state) => state.group.groupId);
+    let groupName = useSelector((state) => state.group.name);
+    let groupMemberIds = useSelector((state) => state.group.memberIds);
+
+    useEffect(() => {
+        //if a group hasn't already been stored, or if the stored groupId is different than the one passed to Group as a route parameter
+        if(!storedGroupId || storedGroupId !== groupId) {
+            dispatch(fetchGroupData(groupId));
+        }
+    }, [storedGroupId, groupId, dispatch]);
+
+    return (
+        <div>
+            <p>{groupName}</p>
+            <GroupMemberList groupMembersIds={groupMemberIds} />
+            <DeckList listType="group" listId={groupId} />
+        </div>
   )
 }
 

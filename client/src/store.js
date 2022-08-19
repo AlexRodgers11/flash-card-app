c0a25221f71c115fbdc4e50af0e433747875395b
Alex Rodgers <skyewulff@outlook.com>
2022-08-18 21:24:13 -0400

    fix typo in store that was causing the decksSlice reducer to be registered for both deck and decks

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import deckReducer from "./reducers/deckSlice";
 import decksReducer from "./reducers/decksSlice";
 import groupReducer from "./reducers/groupSlice";
 
 export const store = configureStore({
     reducer: {
-        deck: decksReducer,
+        deck: deckReducer,
         decks: decksReducer,
         group: groupReducer,
     }

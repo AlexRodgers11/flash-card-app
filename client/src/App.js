d9fe774871ac3cf42d0e3d4bdea8f027a6f75e46
Alex Rodgers <skyewulff@outlook.com>
2022-08-19 18:14:03 -0400

    add Header component and import it into App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import DeckList from './components/DeckList';
+import Header from './components/Header';
 import Router from './components/Router';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       {/* <DeckList listType="user" listId="62ec99d239500ed261745172" /> */}
       {/* <DeckList listType="category" listId="62ec99d339500ed261745209" /> */}
       {/* <DeckList listType="group" listId="62ec99d339500ed2617451e6" /> */}
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <button>Login</button>
+        <button>Sign Up</button>
+    </div>
+  )
+}
+
+export default Header

aca1ed1066bf24c3698d259972840ba2a899ddbc
Alex Rodgers <skyewulff@outlook.com>
2022-08-19 13:40:32 -0400

    create User component and add it to a route in the Router component

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Deck from './Deck';
 import Group from './Group';
+import User from './User';
 
 function Router() {
+                //next step test user route
+
     return (
         <Routes>
             <Route exact path="/groups/:groupId" element={<Group />} />
             <Route exact path="/decks/:deckId" element={<Deck />} />
+            <Route exact path="/users/:userId" element={<User />} />
         </Routes>
     )
 }
---
+++ client/src/components/User.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import useToggle from '../hooks/useToggle';
+import DeckList from './DeckList';
+
+function User() {
+    const { userId } = useParams(); 
+    const [userData, setUserData] = useState({});
+    const [loaded, toggleLoaded] = useToggle(false);
+    
+
+    useEffect(() => {
+        if(!loaded) {
+            const baseURL = 'http://localhost:8000';
+            axios.get(`${baseURL}/users/${userId}`)
+                .then((response) => {
+                    setUserData(response.data);
+                    toggleLoaded();
+                })
+                .catch((err) => console.log(err));
+        }
+    }, [loaded, toggleLoaded, userId]);
+
+    return (
+        <div>
+            {loaded ? 
+                <div>
+                    <h1>{userData.login.username}</h1>
+                    <p>{userData.photo}</p>
+                    <p>{userData.email}</p>
+                    <DeckList listType="user" listId={userId} />
+                </div>
+                :
+                null
+            }
+            
+        </div>
+    )
+}
+
+export default User

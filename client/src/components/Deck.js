277cfec79067aff6983c6383b0c9d304f523bc0c
Alex Rodgers <skyewulff@outlook.com>
2022-08-18 21:30:04 -0400

    create Deck component and add route to it in Router component

---
+++ client/src/components/Deck.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchDeck } from '../reducers/deckSlice';
+import { useParams } from 'react-router';
+
+function Deck() {
+    // const { deckId, name, publiclyAvailable, creator, cards, permissions } = useSelector((state) => state.deck);
+    const storedDeckId = useSelector((state) => state.deck.deckId);
+    const name = useSelector((state) => state.deck.name);
+    const publiclyAvailable = useSelector((state) => state.deck.publiclyAvailable);
+    const creator = useSelector((state) => state.deck.creator);
+    const cards = useSelector((state) => state.deck.cards);
+    const permissions = useSelector((state) => state.deck.permissions);
+    
+    const { deckId } = useParams();
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        console.log("use effect ran");
+        if(!storedDeckId || storedDeckId !== deckId) {
+            dispatch(fetchDeck(deckId));
+        }
+    }, [deckId, dispatch, storedDeckId]);
+    
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h3>{creator}</h3>
+            <p>Public?: {publiclyAvailable ? "True" : "False"}</p>
+            {cards.map(card => <p>{card}</p>)}
+        </div>
+    )
+}
+
+Deck.propTypes = {
+    deckId: PropTypes.string
+}
+
+export default Deck
--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Deck from './Deck';
 import Group from './Group';
 
 function Router() {
     return (
         <Routes>
             <Route exact path="/groups/:groupId" element={<Group />} />
+            <Route exact path="/decks/:deckId" element={<Deck />} />
         </Routes>
     )
 }

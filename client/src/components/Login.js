261c7f57031f575b07eb1a79a5429ede06d8a3ac
Alex Rodgers <skyewulff@outlook.com>
2022-08-21 13:48:57 -0400

    set Login component up to dispatch actions to get JWT and user data then redirect to Dashboard component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router'
 import useFormInput from '../hooks/useFormInput';
+import { fetchLoggedInUserData, login } from '../reducers/loginSlice';
 
 function Login() {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const [usernameOrEmail, clearUsernameOrEmail, setUsernameOrEmail] = useFormInput('');
     const [password, clearPassword, setPassword] = useFormInput('');
+    const userId = useSelector((state) => state.login.userId);
+    const foundUser = useSelector((state) => state.login.username);
 
     const handleSubmit = evt => {
         evt.preventDefault();
+        dispatch(login({usernameOrEmail, password}));
+        clearPassword();
+        clearUsernameOrEmail();
     }
+
+    useEffect(() => {
+        if(userId) {
+            dispatch(fetchLoggedInUserData(userId));
+        }
+    }, [userId, dispatch]);
+
+    useEffect(() => {
+        if(foundUser) {
+            navigate(`/dashboard`);
+        }
+    }, [foundUser, navigate]);
 
     return (
         <div>
@@ -22,6 +42,7 @@ function Login() {
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" name="password" value={password} onChange={setPassword} />
                 </div> 
+                <button type="submit">Submit</button>
             </form>
         </div>
     )

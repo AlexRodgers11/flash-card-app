0242c1394f0f1486f8efebea96690c253b03e576
Alex Rodgers <skyewulff@outlook.com>
2022-08-29 21:04:02 -0400

    add logic to Header component to show logout and the user's username instead of login/registered if logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
+import { logout } from '../reducers/loginSlice';
 
 function Header() {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const username = useSelector((state) => state.login.username);
 
     const handleClick = evt => {
         navigate(`/${evt.target.value}`);
     }
 
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate("/login");
+    }
+
 
     return (
-    <div>
-        <button value="login" onClick={handleClick}>Login</button>
-        <button value="register/signup" onClick={handleClick}>Sign Up</button>
-    </div>
+        <div style={{display: "flex", justifyContent: "space-between", padding: ".5em 2em"}}>
+            <div><strong>{username ? username: null}</strong></div>
+            {!username ? 
+                <div>
+                    <button value="login" onClick={handleClick}>Login</button>
+                    <button value="register/signup" onClick={handleClick}>Sign Up</button>
+                </div>
+                :
+                <div>
+                    <button value="logout" onClick={handleLogout}>Logout</button>
+                </div>
+            }
+            
+        </div>
   )
 }
 

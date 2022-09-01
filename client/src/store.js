f947c6211394e568c8a8e9e36631f666b08fccfa
Alex Rodgers <skyewulff@outlook.com>
2022-09-01 15:24:13 -0400

    register practiceSessionSlice of store in store

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@ import deckReducer from "./reducers/deckSlice";
 import decksReducer from "./reducers/decksSlice";
 import groupReducer from "./reducers/groupSlice";
 import loginReducer from "./reducers/loginSlice";
+import practiceSessionReducer from "./reducers/practiceSessionSlice";
 
 export const store = configureStore({
     reducer: {
         deck: deckReducer,
         decks: decksReducer,
         group: groupReducer,
-        login: loginReducer
+        login: loginReducer,
+        practiceSession: practiceSessionReducer
     }
 });

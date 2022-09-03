c088f1f2c55fd2bf47bd6f7ffc96970283045359
Alex Rodgers <skyewulff@outlook.com>
2022-09-03 19:12:53 -0400

    change position and dimensions of App component to allow for proper modal functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import DeckList from './components/DeckList';
 import Header from './components/Header';
 import Router from './components/Router';
 
@@ -7,9 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <DeckList listType="user" listId="62ec99d239500ed261745172" /> */}
-      {/* <DeckList listType="category" listId="62ec99d339500ed261745209" /> */}
-      {/* <DeckList listType="group" listId="62ec99d339500ed2617451e6" /> */}
       <Router />
     </div>
   );

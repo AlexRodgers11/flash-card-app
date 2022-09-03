40b95d2692b87d261451f3a18e4781d7444b0acb
Alex Rodgers <skyewulff@outlook.com>
2022-09-03 19:07:05 -0400

    create and style Modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Modal.css';
+
+function Modal(props) {
+	return (
+		<div className={`Modal${props.showModal? '_active' : '_hidden'}`}>
+			<div className='Modal_backdrop' onClick={props.hideModal}></div>
+			<div className='Modal_content'>
+				<p className='Modal_close' onClick={props.hideModal}>X</p>
+				{React.cloneElement(props.children,{hideModal: props.hideModal})}
+			</div>
+		</div>
+)
+}
+
+Modal.propTypes = {
+	showModal: PropTypes.bool,
+	hideModal: PropTypes.func
+}
+
+export default Modal

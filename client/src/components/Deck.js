bc736766c69b509995cc1f2328ca8bfe9245f9d8
Alex Rodgers <skyewulff@outlook.com>
2022-09-03 12:36:25 -0400

    add delete functionality and toggleable edit mode state to Deck component

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchDeck } from '../reducers/deckSlice';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import Card from './Card';
+import useToggle from '../hooks/useToggle';
+import axios from 'axios';
+import { deleteDeck } from '../reducers/decksSlice';
+
+const baseURL = 'http://localhost:8000';
 
 function Deck() {
     // const { deckId, name, publiclyAvailable, creator, cards, permissions } = useSelector((state) => state.deck);
@@ -12,10 +17,32 @@ function Deck() {
     const creator = useSelector((state) => state.deck.creator);
     const cards = useSelector((state) => state.deck.cards);
     const permissions = useSelector((state) => state.deck.permissions);
+    const [editMode, toggleEditMode] = useToggle(false);
+    const [deleteInitiated, toggleDeleteInitiated] = useToggle(false);
     
     const { deckId } = useParams();
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const initiateDeleteDeck = () => {
+        toggleDeleteInitiated();
+    }
+
+    const cancelDeleteDeck = () => {
+        toggleDeleteInitiated();
+    };
+
+    const confirmDeleteDeck = () => {
+        axios.delete(`${baseURL}/decks/${deckId}`)
+            .then(() => {
+                dispatch(deleteDeck({deckId}));
+                navigate("/dashboard");
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    };
 
     useEffect(() => {
         console.log("use effect ran");
@@ -26,6 +53,16 @@ function Deck() {
     
     return (
         <div>
+            <button onClick={toggleEditMode}>{editMode ? "Done" : "Edit"}</button>
+            <button onClick={initiateDeleteDeck}>Delete</button>
+            {!deleteInitiated ?
+                null
+                :
+                <div>
+                    <button onClick={confirmDeleteDeck}>Yes</button>
+                    <button onClick={cancelDeleteDeck}>No</button>
+                </div>
+            }
             <h1>{name}</h1>
             <h3>{creator}</h3>
             <p>Public?: {publiclyAvailable ? "True" : "False"}</p>

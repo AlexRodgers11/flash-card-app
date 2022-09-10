1e320f654e16c4dc5029e5c7085d3600f164a5ae
Alex Rodgers <skyewulff@outlook.com>
2022-09-10 14:24:35 -0400

    add more properties to group model

--- server/models/group.js
+++ server/models/group.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema;
 
 const Group = new Schema({
     name: String,
+    creator: {type: Schema.Types.ObjectId, ref: "user"},
+    administrators: [{type: Schema.Types.ObjectId, ref: "user"}],
     members: [{type: Schema.Types.ObjectId, ref: "user"}],
-    decks: [{type: Schema.Types.ObjectId, ref: "deck"}]
+    decks: [{type: Schema.Types.ObjectId, ref: "deck"}],
+    activity: [{type: Schema.Types.ObjectId, ref: "activity"}],
+    joinCode: String
 });
 
 export default mongoose.model("group", Group);

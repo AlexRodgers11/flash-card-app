42ef68f517f7decf82f89ebb28a60b5c62bf2785
Alex Rodgers <skyewulff@outlook.com>
2022-09-15 18:20:57 -0400

    create Modal in Group component for administrators to have option to add one of their decks to the group

--- client/src/components/Group.js
+++ client/src/components/Group.js
@@ -1,18 +1,49 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router'
-import { fetchGroupData } from '../reducers/groupSlice';
+import { useNavigate, useParams } from 'react-router'
+import { addActivity, fetchGroupData } from '../reducers/groupSlice';
 import DeckList from './DeckList';
 import ActivityList from './ActivityList';
 import GroupMemberList from './GroupMemberList';
+import Modal from './Modal';
+import useToggle from '../hooks/useToggle';
+import axios from 'axios';
+import { addDeck } from '../reducers/decksSlice';
+
+const baseURL = 'http://localhost:8000';
 
 function Group() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     let { groupId } = useParams();
+    const [showModal, toggleShowModal] = useToggle(false);
+    let userId = useSelector((state) => state.login.userId);
+    let decks = useSelector((state) => state.login.decks);
     let storedGroupId = useSelector((state) => state.group.groupId);
     let groupName = useSelector((state) => state.group.name);
     let groupMemberIds = useSelector((state) => state.group.memberIds);
+    let administrators = useSelector((state) => state.group.administrators);
     let activityIds = useSelector((state) => state.group.activity);
+    
+    const chooseDeck = evt => {
+        axios.get(`${baseURL}/decks/${evt.target.id}`)
+            .then((response) => {
+                axios.post(`${baseURL}/groups/${groupId}/decks`, response.data)
+                    .then((res) => {
+                        console.log(res);
+                        dispatch(addActivity({activityId: res.data.newActivity}));
+                        dispatch(addDeck({deckId: res.data.newDeck}));
+                        toggleShowModal();
+                    })
+                    .catch(err => console.error(err));
+            })
+            .catch(err => console.error(err));
+        
+    }
+
+    const goToCreateNew = () => {
+        navigate(`/users/${userId}/decks/new`);
+    }
 
     useEffect(() => {
         //if a group hasn't already been stored, or if the stored groupId is different than the one passed to Group as a route parameter
@@ -24,9 +55,19 @@ function Group() {
     return (
         <div>
             <p>{groupName}</p>
+            <h3>Administrators:</h3>
+            <GroupMemberList groupMemberIds={administrators} />
             <h3>Activity:</h3>
             <ActivityList activityIds={activityIds}/>
             <GroupMemberList groupMemberIds={groupMemberIds} />
+            {!administrators.includes(userId) ? null : <button onClick={toggleShowModal}>Add Deck</button>}
+            {!showModal ?
+                null
+                :
+                <Modal hideModal={toggleShowModal}>
+                    {decks.map(deck => <span key={deck._id} id={deck._id} onClick={chooseDeck}>{deck.name}</span>)}<button onClick={goToCreateNew}>Create new deck</button>
+                </Modal>
+            }
             <DeckList listType="group" listId={groupId} />
         </div>
   )

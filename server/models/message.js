4057d91cf98cdf37e595c46b13d0caeddbf0e5c3
Alex Rodgers <skyewulff@outlook.com>
2022-09-17 14:23:18 -0400

    completely redo message model

--- server/models/message.js
+++ server/models/message.js
@@ -3,8 +3,15 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Message = new Schema({
-    sender: {type: Schema.Types.ObjectId, ref: "user"},
-    message: String
+    type: String,
+    sendingUser: {type: Schema.Types.ObjectId, ref: "user"},
+    sendingGroup: {type: Schema.Types.ObjectId, ref: "group"},
+    // receivingUser: {type: Schema.Types.ObjectId, ref: "user"},
+    targetDeck: {type: Schema.Types.ObjectId, ref: "deck"},
+    targetCard: {type: Schema.Types.ObjectId, ref: "card"},
+    targetGroup: {type: Schema.Types.ObjectId, ref: "group"},
+    message: String,
+    read: Boolean
 }, {timestamps: true});
 
 export default mongoose.model("message", Message);

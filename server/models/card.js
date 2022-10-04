fc56f14f02903c2f6c355f6f8903212facae7ed8
Alex Rodgers <skyewulff@outlook.com>
2022-10-03 23:33:51 -0400

    use discriminator to make multiple submodels of card model

--- server/models/card.js
+++ server/models/card.js
@@ -1,20 +1,51 @@
 import mongoose from "mongoose";
 
+// const Schema = mongoose.Schema;
+
+// const Card = new Schema({
+//     creator: {type: Schema.Types.ObjectId, ref: "user"},
+//     type: String,
+//     question: String,
+//     correctAnswer: String,
+//     wrongAnswerOne: String,
+//     wrongAnswerTwo: String,
+//     wrongAnswerThree: String,
+//     hint: String,
+//     stats: {
+//         numberCorrect: Number,
+//         numberIncorrect: Number
+//     }
+// }, {timestamps: true});
+
+// export default mongoose.model("card", Card);
+
 const Schema = mongoose.Schema;
 
 const Card = new Schema({
     creator: {type: Schema.Types.ObjectId, ref: "user"},
-    type: String,
+    // type: String,
     question: String,
     correctAnswer: String,
-    wrongAnswerOne: String,
-    wrongAnswerTwo: String,
-    wrongAnswerThree: String,
     hint: String,
     stats: {
         numberCorrect: Number,
         numberIncorrect: Number
     }
-}, {timestamps: true});
+}, {discriminatorKey: "card-type"});
 
-export default mongoose.model("card", Card);
+const FlashCard = new Card.discriminator("FlashCard", new Schema({
+
+}));
+
+const MultipleChoiceCard = new Card.discriminator("MultipleChoiceCard", new Schema({
+    wrongAnswerOne: String,
+    wrongAnswerTwo: String,
+    wrongAnswerThree: String,
+}), {discriminatorKey: "card-type"});
+
+const TrueFalseCard = new Card.discriminator("TrueFalseCard", new Schema({
+    wrongAnswerOne: String
+}));
+
+export { Card, FlashCard, MultipleChoiceCard, TrueFalseCard };
+

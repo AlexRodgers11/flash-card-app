b86686cb08f02a1f7e2c40d872fdd0d2c14296c8
Alex Rodgers <skyewulff@outlook.com>
2022-10-26 11:31:38 -0400

    add PATCH to allowable methods in response header creation in server file

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 //set response headers
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"

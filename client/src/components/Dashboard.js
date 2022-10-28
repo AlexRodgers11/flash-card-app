d3959b163896338221ab5cb01f961010ae5247a1
Alex Rodgers <skyewulff@outlook.com>
2022-10-28 10:23:32 -0400

    add Modal to Dashboard to show form for searching for new groups to join

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,15 +2,25 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
+import useToggle from '../hooks/useToggle';
 import DeckList from './DeckList';
 import GroupList from './GroupList';
+import Modal from './Modal';
+import RegisterJoinGroupsForm from './RegisterJoinGroupsForm';
 
 function Dashboard() {
     const user = useSelector((state) => state.login);
+    const [showGroupSearchModal, toggleGroupSearchModal] = useToggle(false);
     const navigate = useNavigate();
     
     const goToNewDeckForm = () => {
         navigate(`/users/${user.userId}/decks/new`);
+    }
+
+    const searchGroups = () => {
+        if(!showGroupSearchModal) {
+            toggleGroupSearchModal();
+        }
     }
 
     return (
@@ -23,7 +33,15 @@ function Dashboard() {
                 <button onClick={goToNewDeckForm}>Create New Deck</button>
                 <DeckList listType="user" listId={user.userId} />
                 <h1>Groups:</h1>
+                <button onClick={searchGroups}>Search for Groups to Join</button>
                 <GroupList groupIds={user.groups} />
+                {!showGroupSearchModal ?
+                    null
+                    :
+                    <Modal hideModal={toggleGroupSearchModal}>
+                        <RegisterJoinGroupsForm />
+                    </Modal>
+                }
             </div>            
         </div>
     )

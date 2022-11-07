16b2fd99a02daa49b45682e7cfc710960a30a8f9
Alex Rodgers <skyewulff@outlook.com>
2022-11-07 12:44:06 -0500

    remove unused code in userRouter

--- server/routes/users.js
+++ server/routes/users.js
@@ -102,71 +102,6 @@ userRouter.delete("/:userId", async (req, res, next) => {
     }
 });
 
-// userRouter.delete("/:userId", (req, res, next) => {
-//     User.findByIdAndDelete(req.user._id, (err, user) => {
-//         if(err) {
-//             res.status(500).send("There was an error with your request");
-//             throw err;
-//         } else {
-//             Group.updateMany({members: user._id}, {$pull: {members: user._id}})
-//                 .catch((err) => {
-//                     res.status(500).send("There was an error with your request");
-//                     throw err;
-//                 })
-//                 // .then((err, matchedCount, modifiedCount, upsertedId) => {
-//                 //     //all arguments passed to .then() are undefined
-                
-//                 //     console.log(`Matched count: ${matchedCount}`);
-//                 //     console.log(`Modified count: ${modifiedCount}`);
-//                 //     console.log(`Upserted id: ${upsertedId}`);
-//                 // });
-//                 .then(() => {
-//                     console.log("done removing user from groups");
-//                     Deck.deleteMany({creator: user._id})
-//                         .catch(err => {
-//                             if(err) {
-//                                 res.status(500).send("There was an error with your request");
-//                                 throw err;
-//                             }
-//                         })
-//                         .then(() => {
-//                             console.log("done removing decks created by user");
-//                             Deck.updateMany({"permissions.view": user._id}, {$pull: {"permissions.view": user._id}})
-//                                 .catch(err => {
-//                                     if(err) {
-//                                         res.status(500).send("There was an error with your request");
-//                                         throw err;
-//                                     }
-//                                 })
-//                                 .then(() => {
-//                                     console.log("Done removing user from edit permissions");
-//                                     Deck.updateMany({"permissions.edit": user._id}, {$pull: {"permissions.edit": user._id}})
-//                                         .catch(err => {
-//                                             if(err) {
-//                                                 res.status(500).send("There was an error with your request");
-//                                                 throw err;
-//                                             }
-//                                         })
-//                                         .then(() => {
-//                                             console.log("Done removing user from edit permissions");
-//                                             Attempt.deleteMany({_id: {$in: user.attempts}})
-//                                                 .catch(err => {
-//                                                     if(err) {
-//                                                         res.status(500).send("There was an error with your request");
-//                                                     }
-//                                                 })
-//                                                 .then(() => {
-//                                                     console.log("Done removing user's attempts");
-//                                                     res.status(200).send(user);
-//                                                 });
-//                                         })
-//                                 })
-//                         })
-//                 })    
-//         }
-//     });
-// });
-
 userRouter.put("/:userId", (req, res, next) => {
     User.findByIdAndUpdate(req.user._id, req.body, {new: true}, (err, user) => {
         if(err) {
@@ -198,23 +133,6 @@ userRouter.post("/:userId/decks", async (req, res, next) => {
     }
 });
 
-// userRouter.post("/:userId/decks", (req, res, next) => {
-//     let newDeck = new Deck({
-//         name: req.body.deckName,
-//         public: req.body.public,
-//         creator: req.body.creator,
-//     });
-//     newDeck.save((err, deck) => {
-//         User.findByIdAndUpdate(req.user._id, {$push: {decks: deck}}, (err, user) => {
-//             if(err) {
-//                 res.status(500).send("There was an error with your request");
-//             } else {
-//                 res.status(200).send({_id: newDeck._id, name: newDeck.name});
-//             }
-//         });
-//     });
-// });
-
 userRouter.post("/:userId/messages", async (req, res, next) => {
     try {
         let newMessage = new Message();
@@ -231,33 +149,6 @@ userRouter.post("/:userId/messages", async (req, res, next) => {
         throw receivingUserErr;
     }
 });
-
-// userRouter.post("/:userId/messages", (req, res, next) => {
-//     let newMessage = new Message();
-//     newMessage.type = req.body.type;
-//     newMessage.sendingUser = req.body.sendingUser;
-//     newMessage.targetDeck = req.body.targetDeck;
-//     newMessage.targetGroup = req.body.targetGroup;
-//     newMessage.save((messageSaveErr, message) => {
-//         if(messageSaveErr) {
-//             res.status(500).send("There was an error with your request");
-//             throw messageSaveErr;
-//         }
-//         User.findByIdAndUpdate(req.user._id, {$push: {'messages.received': message}})
-//             .then(() => {
-//                 User.findByIdAndUpdate(message.sendingUser, {$push: {'messages.sent': message}})
-//                     .then(() => res.status(200).send(message))
-//                     .catch(senderUpdateErr => {
-//                         res.status(500).send("There was an error with your request");
-//                         throw(senderUpdateErr);
-//                     });
-//             })
-//             .catch(receivingUserErr => {
-//                 res.status(500).send("There was an error with your request");
-//                 throw receivingUserErr;
-//             });
-//     });
-// });
 
 userRouter.delete("/:userId/messages/:messageId", async (req, res, next) => {
     try {
@@ -278,34 +169,6 @@ userRouter.delete("/:userId/messages/:messageId", async (req, res, next) => {
     }
 });
 
-// userRouter.delete("/:userId/messages/:messageId", (req, res, next) => {
-//     Message.findByIdAndDelete(req.params.messageId)
-//         .then(message => {
-//             if(req.user.messages.sent.findIndex(message._id) > -1) {
-//                 User.findByIdAndUpdate(req.user._id, {$pull: {'messages.sent': message._id}})
-//                     .then(res.status(200).send(message))
-//                     .catch(sentDeleteErr => {
-//                         res.status(500).send("There was an error with your request");
-//                         throw sentDeleteErr;
-//                     })
-//             } else if(req.user.messages.received.findIndex(message._id) > -1) {
-//                 User.findByIdAndUpdate(req.user._id, {$pull: {'messages.received': message._id}})
-//                     .then(res.status(200).send(message))
-//                     .catch(receivedDeleteErr => {
-//                         res.status(500).send("There was an error with your request");
-//                         throw receivedDeleteErr;
-//                     })
-//             } else {
-//                 res.status(404).send("Message not found in user's messages");
-//             }
-            
-//         })
-//         .catch(messageDeleteErr => {
-//             res.status(500).send("There was an error with your request");
-//             throw messageDeleteErr;
-//         });
-// });
-
 userRouter.post("/:userId/notifications", async (req, res, next) => {
     try {
         let newNotification = new Notification();
@@ -325,31 +188,6 @@ userRouter.post("/:userId/notifications", async (req, res, next) => {
     }
 
 });
-
-// userRouter.post("/:userId/notifications", (req, res, next) => {
-//     let newNotification = new Notification();
-//     newNotification.type = req.body.type;
-//     newNotification.content = req.body.content;
-//     newNotification.actor = req.body.actor;
-//     newNotification.groupTarget = req.body.groupTarget;
-//     newNotification.deckTarget = req.body.deckTarget;
-//     newNotification.cardTarget = req.body.cardTarget;
-//     newNotification.read = req.body.read;
-//     newNotification.save((notificationSaveErr, notification) => {
-//         if(notificationSaveErr) {
-//             res.status(500).send("There was an error with your request");
-//             throw notificationSaveErr;
-//         }
-//         User.findByIdAndUpdate(req.user._id, {$push: {notifications: notification}})
-//             .then(notification => {
-//                 res.status(200).send(notification);
-//             })
-//             .catch(userUpdateErr => {
-//                 res.status(500).send("There was an error with your request");
-//                 throw userUpdateErr;
-//             });
-//     });
-// });
 
 userRouter.put("/:userId/notifications", (req, res, next) => {
     Notification.updateMany({_id: {$in: req.user.notifications}}, {$set: {read: true}})
@@ -440,19 +278,6 @@ userRouter.post("/:userId/attempts", async (req, res, next) => {
         throw err;
     }
 });
-// userRouter.post("/:userId/attempts", (req, res, next) => {
-//     let newAttempt = new Attempt(req.body);
-//     newAttempt.save((err, attempt) => {
-//         User.findByIdAndUpdate(req.user._id, {$push: {attempts: attempt}}, (err, user) => {
-//             if(err) {
-//                 res.status(500).send("There was an error with your request");
-//                 throw err;
-//             } else {
-//                 res.status(200).send(attempt);
-//             }
-//         });
-//     });
-// });
 
 userRouter.delete("/:userId/attempts/:attemptId", async (req, res, next) => {
     const attemptId = req.params.attemptId;
@@ -466,24 +291,6 @@ userRouter.delete("/:userId/attempts/:attemptId", async (req, res, next) => {
     }
 });
 
-// userRouter.delete("/:userId/attempts/:attemptId", (req, res, next) => {
-//     User.findByIdAndUpdate(req.user._id, {$pull: {attempts: req.params.attemptId}}, (err, user) => {
-//         if(err) {
-//             res.status(500).send("There was an error with your request");
-//             throw err;
-//         } else {
-//             Attempt.findByIdAndDelete(req.params.attemptId, (err, attempt) => {
-//                 if(err) {
-//                     res.status(500).send("There was an error with your request");
-//                     throw err;
-//                 } else {
-//                     res.status(200).send(attempt);
-//                 } 
-//             });
-//         }
-//     });
-// });
-
 userRouter.delete("/:userId/attempts", async (req, res, next) => {
     try {
         const deletedAttempts = await Attempt.deleteMany({_id: req.user.attempts});
@@ -493,24 +300,6 @@ userRouter.delete("/:userId/attempts", async (req, res, next) => {
         res.status(500).send("There was an error with your request");
     }
 });
-
-// userRouter.delete("/:userId/attempts", (req, res, next) => {
-//     Attempt.deleteMany({_id: req.user.attempts}, (err, deletedAttemptsObj) => {
-//         if(err) {
-//             res.status(500).send("There was an error with your request");
-//             throw err;
-//         } else {
-//             User.findByIdAndUpdate(req.user._id, {$set: {attempts: []}}, (err, user) => {
-//                 if(err) {
-//                     res.status(500).send("There was an error with your request");
-//                     throw err;
-//                 } else {
-//                     res.status(200).send(JSON.stringify(deletedAttemptsObj.deletedCount));
-//                 }
-//             });
-//         }
-//     });
-// });
 
 userRouter.delete("/:userId/decks/:deckId/attempts", async (req, res, next) => {
     try {
@@ -523,32 +312,5 @@ userRouter.delete("/:userId/decks/:deckId/attempts", async (req, res, next) => {
         res.status(500).send("There was an error with your request");
     }
 });
-
-// userRouter.delete("/:userId/decks/:deckId/attempts", (req, res, next) => {
-//     Attempt.find({$and: [{_id: {$in: req.user.attempts}}, {deck: req.params.deckId}]}, (err, attempts) => {
-//         if(err) {
-//             res.status(500).send("There was an error with your request");
-//             throw err;
-//         } else {
-//             let attemptIds = attempts.map(attempt => attempt._id);
-//             Attempt.deleteMany({_id: {$in: attemptIds}}, (err, deletedAttemptsObj) => {
-//                 if(err) {
-//                     res.status(500).send("There was an error with your request");
-//                     throw err;
-//                 } else {
-//                     User.findByIdAndUpdate(req.user._id, {$pull: {attempts: {$in: attemptIds}}}, (err, user) => {
-//                         if(err) {
-//                             res.status(500).send("There was an error with your request");
-//                             throw err;
-//                         } else {
-//                             res.status(200).send(JSON.stringify(deletedAttemptsObj.deletedCount));
-//                         }
-//                     });
-//                 }
-//             });
-//         }
-//     });
-// });
-
 
 export default userRouter;

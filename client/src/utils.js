a7e542c9df7517901c1838757cffb9e2c92b7c6a
Alex Rodgers <skyewulff@outlook.com>
2022-11-08 18:10:57 -0500

    tweak shuffleArray function in client utils to copy the array before shuffling it so the passed array isn't mutated

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,10 +1,11 @@
 ////Durstenfeld shuffle, copied from https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 export const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
+    let arrayCopy = [...array];
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
     }
-    return array;
+    return arrayCopy;
 }
 
 const characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','=','?'];

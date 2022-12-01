0ea70e73ad21429303161f2349cd6823bb6e3c71
Alex Rodgers <skyewulff@outlook.com>
2022-12-01 18:57:49 -0500

    create rootReducer in store to allow for fully clearing state tree upon logout

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import deckReducer from "./reducers/deckSlice";
 import decksReducer from "./reducers/decksSlice";
 import groupReducer from "./reducers/groupSlice";
 import loginReducer from "./reducers/loginSlice";
 import practiceSessionReducer from "./reducers/practiceSessionSlice";
 
-export const store = configureStore({
-    reducer: {
+const combinedReducer = combineReducers(
+    {
         deck: deckReducer,
         decks: decksReducer,
         group: groupReducer,
         login: loginReducer,
         practiceSession: practiceSessionReducer
     }
+);
+
+const rootReducer = (state, action) => {
+    if(action.type === "login/logout") {
+        state = undefined;
+    }
+    return combinedReducer(state, action);
+}
+
+export const store = configureStore({
+    reducer: rootReducer
 });

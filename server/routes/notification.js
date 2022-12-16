f290752e59f0451e0341bae5bc10b563c2f2940c
Alex Rodgers <skyewulff@outlook.com>
2022-12-16 13:37:21 -0500

    fix notification GET route in notificationRouter to account for use of discriminators; add case for JoinDecision

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,7 +1,6 @@
 import express from "express";
 const notificationRouter = express.Router();
-// import Notification from "../models/notification.js";
-        import { Notification }  from "../models/notification.js";
+        import { DeckDecision, JoinDecision, Notification }  from "../models/notification.js";
 
 notificationRouter.param("notificationId", (req, res, next, notificationId) => {
     Notification.findById(notificationId, (err, notification) => {
@@ -20,92 +19,60 @@ notificationRouter.param("notificationId", (req, res, next, notificationId) => {
 });
 
 notificationRouter.get("/:notificationId", (req, res, next) => {
-    Notification.findById(req.notification._id, (err, notification) => {
-        if(err) {
-            res.status(500).send("There was an error with your request");
-            throw err;
-        }
-        switch(notification.type) {
-            case 'DeckDecision':
-                notification.populate(
-                    [
-                        {
-                            path: 'actor',
-                            select: 'login.username name.first name.last'
-                        },
-                        {
-                            path: 'groupTarget',
-                            select: 'name'
-                        },
-                        {
-                            path: 'deckTarget',
-                            select: 'name'
-                        }
-                    ]
-                )
-                .then(notification => {
-                    res.status(200).send(notification);
-                })
-                .catch(deckApprovalPopulationError => {
-                    res.status(500).send("There was an error with your request");
-                    throw deckApprovalPopulationError;
-                });
-                break; 
-            // case 'deck-approved':
-            //     notification.populate(
-            //         [
-            //             {
-            //                 path: 'actor',
-            //                 select: 'login.username name.first name.last'
-            //             },
-            //             {
-            //                 path: 'groupTarget',
-            //                 select: 'name'
-            //             },
-            //             {
-            //                 path: 'deckTarget',
-            //                 select: 'name'
-            //             }
-            //         ]
-            //     )
-            //     .then(notification => {
-            //         res.status(200).send(notification);
-            //     })
-            //     .catch(deckApprovalPopulationError => {
-            //         res.status(500).send("There was an error with your request");
-            //         throw deckApprovalPopulationError;
-            //     });
-            //     break; 
-            // case 'deck-denied':
-            //     notification.populate(
-            //         [
-            //             {
-            //                 path: 'actor',
-            //                 select: 'login.username name.first name.last'
-            //             },
-            //             {
-            //                 path: 'groupTarget',
-            //                 select: 'name'
-            //             },
-            //             {
-            //                 path: 'deckTarget',
-            //                 select: 'name'
-            //             }
-            //         ]
-            //     )
-            //     .then(notification => {
-            //         res.status(200).send(notification);
-            //     })
-            //     .catch(deckApprovalPopulationError => {
-            //         res.status(500).send("There was an error with your request");
-            //         throw deckApprovalPopulationError;
-            //     });
-            //     break;
-            default:
-                res.status(500).send("There was an error with your request");
-                break;
-        }
-    });
+    try {
+        //why didn't instantiating response here work- setting it inside the Notification callback didn't work, remains "test" outside the callback
+        // let response = "test";
+        Notification.findById(req.notification._id, async (err, notification) => {
+            if(err) {
+                res.status(500).send(err.message);
+                throw err;
+            }
+            switch(notification.notificationType) {
+                case 'DeckDecision':
+                    let foundDeckDecisionNotification = await DeckDecision.findById(notification._id);
+                    let populatedDeckDecisionNotification = await foundDeckDecisionNotification.populate(
+                        [
+                                    {
+                                        path: 'actor',
+                                        select: 'login.username name.first name.last'
+                                    },
+                                    {
+                                        path: 'groupTarget',
+                                        select: 'name'
+                                    },
+                                    {
+                                        path: 'deckTarget',
+                                        select: 'name'
+                                    }
+                                ]
+                    );
+                    response = populatedDeckDecisionNotification;
+                    break;
+                case 'JoinDecision':
+                    let foundJoinDecisionNotification = await JoinDecision.findById(notification._id);
+                    let populatedJoinDecisionNotification = await foundJoinDecisionNotification.populate(
+                        [
+                            {
+                                path: 'actor',
+                                select: 'login.username name.first name.last'
+                            },
+                            {
+                                path: 'groupTarget',
+                                select: 'name'
+                            }
+                        ]
+                    );
+                    response = populatedJoinDecisionNotification;
+                    break;
+                default:
+                    res.status(400).send("You are attempting to retrieve an invalid type of notification");
+                    break;
+            }
+            res.status(200).send(response);
+        });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 export default notificationRouter;

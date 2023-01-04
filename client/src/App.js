0a6451cb278e3a224b219e8618b261b442da447f
Alex Rodgers <skyewulff@outlook.com>
2023-01-04 12:35:38 -0500

    use styled component syntax to style App component; delete App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,58 @@
-import './App.css';
 import Header from './components/Header';
 import Router from './components/Router';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+	text-align: center;
+	position: relative;
+	height: 100%;
+	width: 100%;
+	& h1, h2, h3, h4, h5, h6, p {
+	  margin-bottom: 0;
+	}
+	& a, a:hover {
+		text-decoration: none;
+		color: inherit
+	}
+	& * {
+		margin-block-start: 0;
+		margin-block-end: 0;
+		margin-inline-start: 0;
+		margin-inline-end: 0;
+	}
+	& *::marker {
+		content: none;
+	}
+`
+
+const HeaderContainer = styled.div`
+	height: 4.5rem
+`
+
+const MainContainer = styled.div`
+	height: calc(100vh - 4.5rem); 
+	minHeight: 25rem
+`
+
+const FooterContainer = styled.div`
+	height: 15vh; 
+	minHeight: 15rem
+`
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Router />
-    </div>
-  );
+	return (
+		<AppWrapper>
+			<HeaderContainer>
+				<Header />
+			</HeaderContainer>
+			<MainContainer>
+				<Router />
+			</MainContainer>
+			<FooterContainer>
+				Footer will go here
+			</FooterContainer>
+		</AppWrapper>
+	);
 }
 
 export default App;

729059fc13ad6f71be32e949a1d2c5adb2abce2a
Alex Rodgers <skyewulff@outlook.com>
2023-01-16 20:59:33 -0500

    style control bar in BrowseDecks

--- client/src/components/BrowseDecks.js
+++ client/src/components/BrowseDecks.js
@@ -7,7 +7,49 @@ import styled from "styled-components";
 const baseURL = 'http://localhost:8000';
 
 const ControlBarWrapper = styled.form`
-        
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 3rem;
+    background-color: #474747;        
+    color: white;
+    font-size: 1.25rem;
+    & label {
+        margin-right: .35rem;
+        @media (max-width: 690px) {
+            margin-right: .25rem;
+        }
+    }
+    & input, select {
+        margin-right: .75rem ;
+        @media (max-width: 690px) {
+            margin-right: .15rem;
+        }
+    }
+    // @media (max-width: 765px) {
+    //     font-size: .85rem;
+    // }
+    @media (max-width: 960px) {
+        font-size: 1rem;       
+    @media (max-width: 790px) {
+        font-size: .85rem;
+    }
+    @media (max-width: 690px) {
+        font-size: .75rem;
+    }
+    @media (max-width: 625px) {
+        font-size: .65rem;
+    }
+    @media (max-width: 515px) {
+        font-size: .85rem;
+        height: 5rem;
+        flex-direction: column;
+        justify-content: center;
+        & input, select {
+            margin: .05rem 0;
+        }
+    }
 `;
 
 const StyledInfiniteScroll = styled(InfiniteScroll)`
@@ -192,21 +234,26 @@ function BrowseDecks() {
     return (
         <div>
             <ControlBarWrapper>
-                <label htmlFor="search">Search for decks</label>
-                <input type="text" name="searchString" id="searchString" onChange={handleChangeCriteria} value={criteria.search} data-selection_type="searchString" />
+                <div>
+                    <label htmlFor="search">Search for decks</label>
+                    <input type="text" name="searchString" id="searchString" onChange={handleChangeCriteria} value={criteria.search} data-selection_type="searchString" />
+                </div>
                 <select name="category" id="category" onChange={handleChangeCriteria} value={criteria.categoryId} data-selection_type="categoryId" >
                     <option value="">Filter By Category</option>
                     {categories.length > 0 && categories.map(category => <option key={category._id} value={category._id}>{category.name}</option>)}
                 </select>
-                <select name="sort" id="sort" onChange={handleSortChange} value={sort} >
-                    <option value="">Sort</option>
-                    <option value="a-z">A-Z</option>
-                    <option value="z-a">Z-A</option>
-                    <option value="newest">Newest</option>
-                    <option value="oldest">Oldest</option>
-                    <option value="card-count-up">Most Cards</option>
-                    <option value="card-count-down">Least Cards</option>
-                </select>
+                <div>
+                    <label htmlFor="sort">Sort</label>
+                    <select name="sort" id="sort" onChange={handleSortChange} value={sort} >
+                        <option value="">Sort</option>
+                        <option value="a-z">A-Z</option>
+                        <option value="z-a">Z-A</option>
+                        <option value="newest">Newest to Oldest</option>
+                        <option value="oldest">Oldest to Newest</option>
+                        <option value="card-count-up">Most to Least Cards</option>
+                        <option value="card-count-down">Least to Most Cards</option>
+                    </select>
+                </div>
             </ControlBarWrapper>
             <StyledInfiniteScroll
                 dataLength={decks.length}

62828c0035d5500ed9d2084be80c9bdf1025e667
Alex Rodgers <skyewulff@outlook.com>
2023-01-21 22:04:44 -0500

    add indication on PracticeLaunchPage and UserDecksPage to show there are no decks, including a button to create a new one

--- client/src/components/PracticeLaunchPage.js
+++ client/src/components/PracticeLaunchPage.js
@@ -1,14 +1,42 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import DeckList from "./DeckList";
+import styled from "styled-components";
+
+const NoDecksMessage = styled.div`
+    margin-top: 2rem;
+    font-size: 1.5rem;
+    font-style: italic; 
+    color: #616161;
+    margin-bottom: 1rem;
+    @media (max-width: 400px) {
+        font-size: 1.25rem;
+    }
+`;
+
+const StyledButton = styled.button`
+    margin: 4rem 0 3rem 0;
+    width: 14rem;
+    height: 5rem;
+    border-radius: 1rem;
+`;
 
 function PracticeLaunchPage() {
     const decks = useSelector((state) => state.login.decks);
     const userId = useSelector((state) => state.login.userId);
+
     return (
         <div>
-            <DeckList listType="user" listId={userId} />
+            {decks.length ? 
+                <DeckList listType="user" listId={userId} />
+                :
+                <>
+                    <NoDecksMessage>
+                        <p>No decks have been created yet</p>
+                    </NoDecksMessage>
+                    <Link to={`/users/${userId}/decks/new`}><StyledButton>Create New Deck</StyledButton></Link>
+                </>
+            }
         </div>
     );
 }
--- client/src/components/UserDecksPage.js
+++ client/src/components/UserDecksPage.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -7,19 +8,33 @@ const UserDecksPageWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-`
+`;
 
 const StyledButton = styled.button`
     margin: 4rem 0 3rem 0;
     width: 14rem;
     height: 5rem;
     border-radius: 1rem;
-`
+`;
+
+const NoDecksMessage = styled.div`
+    margin-top: 2rem;
+    font-size: 1.5rem;
+    font-style: italic; 
+    color: #616161;
+    margin-bottom: 1rem;
+    @media (max-width: 400px) {
+        font-size: 1.25rem;
+    }
+`;
+
 
 function UserDecksPage() {
     const { userId } = useParams(); 
+    const decks = useSelector((state) => state.login.decks)
     return (
         <UserDecksPageWrapper className="UserDecksPageWrapper">
+            {!decks.length &&  <NoDecksMessage><p>No decks have been created yet</p></NoDecksMessage>}
             <Link to={`/users/${userId}/decks/new`}><StyledButton>Create New Deck</StyledButton></Link>
             <DeckList listType="user" listId={userId} />
         </UserDecksPageWrapper>

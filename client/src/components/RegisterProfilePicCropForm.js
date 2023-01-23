fe974142569d7a4fa191e6c57408dff1cc08529f
Alex Rodgers <skyewulff@outlook.com>
2023-01-23 16:06:51 -0500

    set up cropping functionality in RegisterProfilePicCropForm.js

--- client/src/components/RegisterProfilePicCropForm.js
+++ client/src/components/RegisterProfilePicCropForm.js
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import Cropper from "react-easy-crop";
 import styled from "styled-components";
-import useFormInput from "../hooks/useFormInput";
-
+import { getCroppedImg, urlToFile } from "../utils";
+import { updateProfilePic } from "../reducers/loginSlice";
 
 const CropContainer = styled.div`
-position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 80px;`;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 80px;
+`;
 
 const RegisterProfilePicCropFormWrapper = styled.div`
-    width: 50vw;
-    height: 50vh;
+    width: 500px;
+    height: 500px;
+    // width: 50vw;
+    // height: 50vh;
 `;
 
 const ControlsWrapper = styled.div`
@@ -33,51 +36,75 @@ const ControlsWrapper = styled.div`
     }
 `;
 
+const StyledCropper = styled(Cropper)`
+    // width: 80%;
+    // height: 80%;
+`; 
+
+const StyledCropContainer = styled(CropContainer)`
+    // width: 100%;
+    // height: 100%;
+`;
+
+    
 function RegisterProfilePicCropForm() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
     const photo = useSelector((state) => state.login.photo);
     const [crop, setCrop] = useState({x: 0, y: 0});
-    // const [zoom, clearZoom, handleZoomChange] = useFormInput(1);
-    const [zoom, setZoom] = useState(3);
-    // const [aspect, clearAspect, handleAspectChange] = useFormInput(1)
+    const [zoom, setZoom] = useState(1);
     const [aspect, setAspect] = useState(1);
+    const [rotation, setRotation] = useState(0);
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+    const dispatch = useDispatch();
+    const userId = useSelector((state) => state.login.userId);
+    
+    const handleCropComplete = useCallback((croppedArea, croppedAreaPixelsArg) => {
+        setCroppedAreaPixels(croppedAreaPixelsArg);
+    }, []);
 
-    const handleCropChange = (crop) => {
-        setCrop({crop});
-    }
+    const saveCroppedImage = useCallback(async () => {
+        try {
+            const croppedImage = await getCroppedImg(
+                photo,
+                croppedAreaPixels,
+                rotation
+            );
+            console.log("donee", { croppedImage });
+            console.log(croppedImage);
+            console.log(typeof croppedImage);
+            console.log(croppedImage instanceof Blob);
+            const fileFromURL = urlToFile(croppedImage);
 
-    const handleCropComplete = (croppedArea, croppedAreaPixels) => {
-        console.log(croppedAreaPixels.width / croppedAreaPixels.height);
-    }
-
-    // const handleZoomChange = (evt, zoom) => {
-    const handleZoomChange = (evt) => {
-        // console.log({zoom});
-        console.log({evt});
-        // setZoom(zoom);
-        setZoom(evt.target.value);
-    }
+            dispatch(updateProfilePic({userId: userId, photo: fileFromURL}));
+          
+        } catch (e) {
+          console.error(e);
+        }
+      }, [croppedAreaPixels, rotation, photo]);
 
     return (
         <RegisterProfilePicCropFormWrapper>
-            <CropContainer>
-                <Cropper 
+            <StyledCropContainer>
+                <StyledCropper 
                     image={photo}
                     crop={crop}
                     zoom={zoom}
                     aspect={aspect}
+                    rotation={rotation}
                     cropShape="round"
-                    showGrid={true}
-                    onCropChange={handleCropChange}
+                    zoomWithScroll={true}
+                    cropSize={{height: 175, width: 175}}
+                    onCropChange={setCrop}
                     onCropComplete={handleCropComplete}
-                    onZoomChange={handleZoomChange}
+                    onZoomChange={setZoom}
+                    onRotation={setRotation}
                 />
-            </CropContainer>
+            </StyledCropContainer>
             <ControlsWrapper>
-                {/* <input type="range" min={1} max={3} step={0.1} value={zoom} className="slider" onChange={(e, zoom) => handleZoomChange(zoom)} /> */}
-                {/* <input type="range" min={1} max={3} step={0.1} value={zoom} className="slider" onChange={(evt) => handleZoomChange(evt)} /> */}
-                <input type="range" min={1} max={3} step={0.1} value={zoom} className="slider" onChange={handleZoomChange} />
+                <label htmlFor="size-slider">Size</label>
+                <input type="range" min={.01} max={3} step={0.01} value={zoom} className="slider" onChange={(evt) => setZoom(evt.target.value)} />
+                <label htmlFor="rotation-slider">Rotation</label>
+                <input type="range" id="rotation-slider" min={0} max={360} step={1} value={rotation} className="slider" onChange={(evt) => setRotation(evt.target.value)} />
+                <button onClick={saveCroppedImage}>Crop and Save</button>
             </ControlsWrapper>
         </RegisterProfilePicCropFormWrapper>
     )

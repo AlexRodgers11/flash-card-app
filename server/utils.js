4be8e8930cba7aea5ad1a9e95d2241874b7e1153
Alex Rodgers <skyewulff@outlook.com>
2023-01-25 18:39:24 -0500

    create function in server utils that creates a copy of a deck

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,7 @@
+import { FlashCard, MultipleChoiceCard, TrueFalseCard } from "./models/card.js";
+import Deck from "./models/deck.js";
+import mongoose from "mongoose";
+
 import crypto from "crypto";
 
 const characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','=','?'];
@@ -39,3 +43,62 @@ export const swapIndexes = (arr, idx1, idx2) => {
 export default getRandomCardType;
 
 export const generateRandomFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
+
+export const copyDeck = async (deckId) => {
+    try {
+        const foundDeck = await Deck.findById(deckId).populate("cards");
+        
+        const deckCopy = new Deck({
+            name: foundDeck.name,
+            publiclyAvailable: false,
+            creator: foundDeck.creator,
+            attempts: [],
+            categories: foundDeck.categories.slice(),
+            cards: await Promise.all(foundDeck.cards.map(async card => {
+                let copiedCard;
+                switch(card.cardType) {
+                    case "FlashCard":
+                        copiedCard = new FlashCard({
+                            creator: card.creator,
+                            question: card.question,
+                            correctAnswer: card.correctAnswer,
+                            hint: card.hint,
+                            attempts: [],
+                        })
+                        break;
+                        case "TrueFalseCard":
+                            copiedCard = new TrueFalseCard({
+                            creator: card.creator,
+                            question: card.question,
+                            correctAnswer: card.correctAnswer,
+                            wrongAnswerOne: card.wrongAnswerOne,
+                            hint: card.hint,
+                            attempts: [],
+                        });
+                        break;
+                    case "MultipleChoiceCard":
+                        copiedCard = new MultipleChoiceCard({
+                            creator: card.creator,
+                            question: card.question,
+                            correctAnswer: card.correctAnswer,
+                            wrongAnswerOne: card.wrongAnswerOne,
+                            wrongAnswerTwo: card.wrongAnswerTwo,
+                            wrongAnswerThree: card.wrongAnswerThree,
+                            hint: card.hint,
+                            attempts: [],
+                        });
+                        break;
+                    default: 
+                        break;
+                    }
+                    copiedCard._id = new mongoose.Types.ObjectId()
+                    const savedCardCopy = await copiedCard.save();
+                    return savedCardCopy;
+                }))
+            });
+        await Promise.all(deckCopy.cards);
+        return deckCopy;
+    } catch (err) {
+        console.error(err);
+    }
+}

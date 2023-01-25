961f8261eb1041db3ef7b90c7606244d6aced661
Alex Rodgers <skyewulff@outlook.com>
2023-01-24 23:42:29 -0500

    add two new properties to Deck model to aid in view permissions for group decks

--- server/models/deck.js
+++ server/models/deck.js
@@ -10,12 +10,11 @@ const Deck = new Schema({
     cards: [{type: Schema.Types.ObjectId, ref: "Card"}],
     //didn't have this because when retrieving decks of a category can just search that category. The problem is if I want to display on the deck what categories it belongs to have to search ALL categories for ALL decks, which would be too long
     categories: [{type: Schema.Types.ObjectId, ref: "Category"}],
-    permissions: {
-        view: [{type: Schema.Types.ObjectId, ref: "User"}],
-        edit: [{type: Schema.Types.ObjectId, ref: "User"}],
-        copy: Boolean,
-        suggest: Boolean
-    }
+    //these two new properties allow for a submitted deck to be private and unsearchable by BrowseDecks, but allow admins to still view it. It also allows for group decks to be private but members still be able to see it
+    groupDeckBelongsTo: {type: Schema.Types.ObjectId, ref: "Group"},
+    approvedByGroupAdmins: Boolean
+
+
 }, {timestamps: true});
 
 export default mongoose.model("Deck", Deck);

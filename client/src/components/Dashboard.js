bdd3ac4d126ef0590da928aae18292d3111adbb4
Alex Rodgers <skyewulff@outlook.com>
2023-01-29 15:35:16 -0500

    add icons and a little more styling to Dashboard buttons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { GiBookCover, GiStack } from "react-icons/gi";
+import { HiUserGroup } from "react-icons/hi";
+import { HiRectangleStack } from "react-icons/hi2";
+import { SiBookstack } from "react-icons/si";
+import { GoGraph } from "react-icons/go";
+import { ImSearch } from "react-icons/im";
+import { RiUserSettingsFill } from "react-icons/ri";
 
 const StyledLink = styled(Link)`
     display: flex; 
     margin: 5vw;
+    padding: .25rem;
     height: 24vw;
     width: 24vw;
     max-height: 280px;
@@ -13,14 +21,25 @@ const StyledLink = styled(Link)`
     min-height: 150px;
     min-width: 150px;
     border: 1px solid black; 
+    border-radius: 5%;
     align-items: center; 
     justify-content: center; 
     text-decoration: none; 
     color: black; 
+    & .hover-text {
+        display: none;
+        font-size: 1.75rem;
+    }
     &:hover {
         background-color: black;
         color: white !important;
     }    
+    &:hover .icon {
+        display: none;
+    }
+    &:hover .hover-text {
+        display: block;
+    }
     @media (max-width: 762px) {
         height: 30vw;
         width: 30vw;
@@ -61,12 +80,13 @@ function Dashboard() {
     return (
         <DashboardWrapper className="Dashboard">
             <Grid>
-                <StyledLink to="/decks/public" >Browse All Decks</StyledLink>
-                <StyledLink to={`/users/${user.userId}/decks`} >Your Decks</StyledLink>
-                <StyledLink to={`/users/${user.userId}/decks/new`} >Create a New Deck</StyledLink>
-                <StyledLink to={`/users/${user.userId}/groups`} >Your Groups</StyledLink>
-                <StyledLink to={`/users/${user.userId}/practice`} >Practice</StyledLink>
-                <StyledLink to={`/users/${user.userId}/statistics/sessions`} >Stats</StyledLink>
+                <StyledLink to="/decks/public" ><ImSearch className="icon" size="7rem" /><p className="hover-text">Search Public Decks</p></StyledLink>
+                <StyledLink to={`/users/${user.userId}/decks`} ><HiRectangleStack className="icon" size="7rem" /><p className="hover-text">Your Decks</p></StyledLink>
+                <StyledLink to={`/users/${user.userId}/decks/new`} ><RiUserSettingsFill className="icon" size = "7rem" /><p className="hover-text">Profile Settings</p></StyledLink>
+                <StyledLink to={`/users/${user.userId}/groups`} ><HiUserGroup className="icon" size="7rem"/><p className="hover-text">Your Groups</p></StyledLink>
+                {/* <StyledLink to={`/users/${user.userId}/groups`} ><HiUserGroup className="icon" size="7rem" color="blue"/><p className="hover-text">Your Groups</p></StyledLink> */}
+                <StyledLink to={`/users/${user.userId}/practice`} ><GiBookCover className="icon" size="7rem" /><p className="hover-text">Practice</p></StyledLink>
+                <StyledLink to={`/users/${user.userId}/statistics/sessions`} ><GoGraph className="icon" size="7rem" /><p className="hover-text">Statistics</p></StyledLink>
             </Grid>
         </DashboardWrapper>
     )

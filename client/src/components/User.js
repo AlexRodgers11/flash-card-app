66b07b33060e7fd7acdc1903dfcdb264c98c1462
Alex Rodgers <skyewulff@outlook.com>
2023-01-30 12:47:34 -0500

    fix bug in User component that prevents fetching new user's data if there is another user's data in the store

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,43 +1,40 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import useToggle from '../hooks/useToggle';
 import DeckList from './DeckList';
 
 function User() {
     const { userId } = useParams(); 
     const [userData, setUserData] = useState({});
-    const [loaded, toggleLoaded] = useToggle(false);
     
     const baseURL = 'http://localhost:8000';
 
     useEffect(() => {
-        if(!loaded) {
-            axios.get(`${baseURL}/users/${userId}`)
-                .then((response) => {
-                    setUserData(response.data);
-                    toggleLoaded();
-                })
-                .catch((err) => console.log(err));
+        if(userData._id !== userId) {
+            (async () => {
+                try {
+                    const userDataResponse = await axios.get(`${baseURL}/users/${userId}`);
+                    setUserData(userDataResponse.data);
+                } catch (err) {
+                    console.error(err);
+                }
+            })();
         }
-    }, [loaded, toggleLoaded, userId]);
+    }, [userData._id, userId]);
 
-    return (
-        <div>
-            {loaded ? 
-                <div>
-                    <h1>{userData.login?.username}</h1>
-                    <p>{userData.login.password}</p>
-                    <p>{userData.photo}</p>
-                    <p>{userData.login.email}</p>
-                    <DeckList listType="user" listId={userId} />
-                </div>
-                :
-                null
-            }
-            
-        </div>
-    )
+    if(userData._id !== userId) {
+        return <>Loading</>
+    } else {
+        return (
+            <div>
+                <h1>{userData.login?.username}</h1>
+                <p>{userData.login.password}</p>
+                <p>{userData.photo}</p>
+                <p>{userData.login.email}</p>
+                <DeckList listType="user" listId={userId} />
+            </div>
+        );
+    }
 }
 
 export default User

00278fc9b4ccf9697557b573787a4d5a2f0f14e3
Alex Rodgers <skyewulff@outlook.com>
2023-02-04 21:42:52 -0500

    restyle Logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -2,34 +2,44 @@ import styled from "styled-components";
 
 const LogoWrapper = styled.div`
     position: relative;
-    height: 52px;
-    bottom: 2px;
-    background-color: white;
+    display: flex;
+    align-items: center;
+    // height: 52px;
+    height: 5.5rem;
+    // bottom: 2px;
     &:hover div{
         background-color: #fafafa;
         color: #363636;
     }
     & div {
         position: relative;
-        display: flex;
+        // display: flex;
+        display: inline-flex;
         justify-content: center;
         align-items: center;
         box-sizing: border-box;
         width: 42px;
-        height: 52.125px;
-        border: 2px solid black;
+        height: 56px;
+        // width: 42px;
+        // height: 52.125px;
+        border: 1px solid black;
         background-color: white;
-        font-size: 0.6em;
+        border-radius: 6px;
+        // font-size: 0.6em;
+        font-size: 0.7em;
         font-weight: 900;
         font-family: "Noto Sans Mono";
         color: black;
         &:first-of-type {
             z-index: 2;
-            margin-right: 36.5px;
+            // margin-right: 36.5px;
+            // bottom: 5px;
+            bottom: 4px;
         }
         &:nth-of-type(2) {
-            bottom: 46px;
-            left: 36.5px;
+            // bottom: 52px;
+            top: 4px;
+            right: 4px;
         }
     
 `;

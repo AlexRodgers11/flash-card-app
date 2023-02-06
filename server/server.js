274aeda08b873ff7aa11062b3199847398e7cb8f
Alex Rodgers <skyewulff@outlook.com>
2023-02-05 20:45:52 -0500

    add Authorization as allowable request header in server

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     next();
 });

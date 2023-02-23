1c3f8a4606ab838578f114767b65394b344328c1
Alex Rodgers <skyewulff@outlook.com>
2023-02-22 22:12:30 -0500

    fix styling of Link in UserDecksPage

--- client/src/components/UserDecksPage.js
+++ client/src/components/UserDecksPage.js
@@ -9,9 +9,7 @@ const UserDecksPageWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     background-color: #52B2FF; 
-    // background-color: #4C4C9D; --darker blue
-
-
+    // background-color: #4C4C9D;
     min-height: calc(100vh - 5.5rem);
 `;
 
@@ -33,6 +31,17 @@ const NoDecksMessage = styled.div`
     }
 `;
 
+const StyledLink = styled(Link)`
+    font-size: 1rem;
+    padding: 2rem;
+    margin: 4rem 0 3rem 0;
+    width: 14rem;
+    // height: 5rem;
+    border-radius: 1rem;
+    border: 2px solid black;
+    background-color: lightgrey;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 3px 0px;
+`;
 
 function UserDecksPage() {
     const { userId } = useParams(); 
@@ -40,7 +49,8 @@ function UserDecksPage() {
     return (
         <UserDecksPageWrapper className="UserDecksPageWrapper">
             {!decks.length &&  <NoDecksMessage><p>No decks have been created yet</p></NoDecksMessage>}
-            <Link to={`/users/${userId}/decks/new`}><StyledButton>Create New Deck</StyledButton></Link>
+            {/* <Link to={`/users/${userId}/decks/new`}><StyledButton>Create New Deck</StyledButton></Link> */}
+            <StyledLink to={`/users/${userId}/decks/new`}>Create New Deck</StyledLink>
             <DeckList listType="user" listId={userId} />
         </UserDecksPageWrapper>
     );

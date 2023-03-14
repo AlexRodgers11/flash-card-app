04c911b880c06000454e5979035afdaabcbd9fab
Alex Rodgers <skyewulff@outlook.com>
2023-03-13 20:41:10 -0400

    add responsive styling to EmptyIndicator styled component

--- client/src/components/StyledComponents/EmptyIndicator.js
+++ client/src/components/StyledComponents/EmptyIndicator.js
@@ -11,10 +11,14 @@ export const EmptyIndicator = styled.div`
     border: 2px solid black;
     font-size: 4rem;
     margin-bottom: 4rem;
+    margin-top: ${(props) => props.marginTop ? `${props.marginTop}rem` : 0};
     @media (max-width: 750px) {
         font-size: 3.25rem;
     }
-    @media screen and (max-width: 400px), (max-height: 600px) {
+    @media (max-width: 400px), (max-height: 600px) {
         font-size: 2.5rem;
+    }
+    @media (max-width: 550px) {
+        margin-top: ${(props) => props.marginTop ? `${props.marginTop / 2}rem` : 0};
     }
 `;

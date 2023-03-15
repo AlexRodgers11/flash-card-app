b27aba3d4fc3ee740a9816cb6729407307bfc805
Alex Rodgers <skyewulff@outlook.com>
2023-03-15 16:59:35 -0400

    remove commented out lines in communicationsRouter

--- server/routes/communications.js
+++ server/routes/communications.js
@@ -7,7 +7,6 @@ const communicationRouter = express.Router();
 communicationRouter.get("/", getUserIdFromJWTToken, async (req, res, next) => {
     try {
         const foundUser = await User.findById(req.userId, "messages notifications -_id")
-        // const foundUser = await User.findById("63db29f6827871ce88c61064", "messages notifications -_id")
             .populate("messages.received", "messageType read")
             .populate("messages.sent", "messageType read")
             .populate("notifications", "notificationType read");

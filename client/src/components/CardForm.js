f6f16f93c3b37f0cb3bf01f9ab1870f41fd058f5
Alex Rodgers <skyewulff@outlook.com>
2023-04-03 23:37:12 -0400

    make relevant input fields required in CardForm

--- client/src/components/CardForm.js
+++ client/src/components/CardForm.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import axios from 'axios';
 import useFormInput from '../hooks/useFormInput';
 import { client } from '../utils';
 
@@ -84,7 +82,7 @@ function CardForm(props) {
 					</div>
 					<div>
 						<label htmlFor='question'>Question</label>
-						<textarea type='text' name='question' id='question' value={question} onChange={handleChangeQuestion} />
+						<textarea required type='text' name='question' id='question' value={question} onChange={handleChangeQuestion} />
 					</div>
 					<div>
 						<label htmlFor='hint'>Hint (optional)</label>
@@ -97,6 +95,7 @@ function CardForm(props) {
 								<div id="true-false-options">
 									<input 
 										type="radio" 
+										required
 										id="correct-answer-true" 
 										name="true-false-correct-answer" 
 										value="True" checked={correctAnswer === "True"} 
@@ -105,6 +104,7 @@ function CardForm(props) {
 									<label htmlFor="correct-answer-true">True</label>
 									<input 
 										type="radio" 
+										required
 										id="correct-answer-false" 
 										name="true-false-correct-answer" 
 										value="False" 
@@ -117,7 +117,7 @@ function CardForm(props) {
 							:
 							<>
 								<label htmlFor="correct-answer">{cardType === 'MultipleChoiceCard' ? 'Correct ' : ''}Answer</label>
-								<textarea id="correct-answer" name="correct-answer" value={correctAnswer} onChange={handleChangeCorrectAnswer} />
+								<textarea required id="correct-answer" name="correct-answer" value={correctAnswer} onChange={handleChangeCorrectAnswer} />
 							</>
 						} 
 					</div>
@@ -127,15 +127,15 @@ function CardForm(props) {
 						<div>
 							<div>
 								<label htmlFor="wrong-answer-one">Wrong Answer #1</label>
-								<textarea id="wrong-answer-one" name="wrong-answer-one" value={wrongAnswerOne} onChange={handleChangeWrongAnswerOne} />
+								<textarea required id="wrong-answer-one" name="wrong-answer-one" value={wrongAnswerOne} onChange={handleChangeWrongAnswerOne} />
 							</div>
 							<div>
 								<label htmlFor="wrong-answer-two">Wrong Answer #2</label>
-								<textarea id="wrong-answer-two" name="wrong-answer-two" value={wrongAnswerTwo} onChange={handleChangeWrongAnswerTwo} />
+								<textarea required id="wrong-answer-two" name="wrong-answer-two" value={wrongAnswerTwo} onChange={handleChangeWrongAnswerTwo} />
 							</div>
 							<div>
 								<label htmlFor="wrong-answer-three">Wrong Answer #3</label>
-								<textarea id="wrong-answer-three" name="wrong-answer-three" value={wrongAnswerThree} onChange={handleChangeWrongAnswerThree} />
+								<textarea required id="wrong-answer-three" name="wrong-answer-three" value={wrongAnswerThree} onChange={handleChangeWrongAnswerThree} />
 							</div>
 						</div>
 					}

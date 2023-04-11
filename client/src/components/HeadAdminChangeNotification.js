7c2a1af4b35cb0e85c6eb4307a96141b93f166c4
Alex Rodgers <skyewulff@outlook.com>
2023-04-11 16:41:54 -0400

    fix typo in HeadAdminChangeNotification that prevents rendering if the group has since been deleted

--- client/src/components/HeadAdminChangeNotification.js
+++ client/src/components/HeadAdminChangeNotification.js
@@ -11,7 +11,7 @@ const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
 function HeadAdminChangeNotification(props) {
     const userId = useSelector((state) => state.login.userId);
     const [group, setGroup] = useState();
-    const [previousHeadAdmin, setpreviousHeadAdmin] = useState();
+    const [previousHeadAdmin, setPreviousHeadAdmin] = useState();
     const [newHeadAdmin, setNewHeadAdmin] = useState();
     const [loading, setLoading] = useState(true);
 	const navigate = useNavigate();
@@ -22,7 +22,7 @@ function HeadAdminChangeNotification(props) {
                 try {
                     const notificationRetrievalResponse = await client.get(`${baseURL}/notifications/${props.notificationId}?type=HeadAdminChange`);
                     setGroup(notificationRetrievalResponse.data.targetGroup);
-                    setpreviousHeadAdmin(notificationRetrievalResponse.data.previousHeadAdmin);
+                    setPreviousHeadAdmin(notificationRetrievalResponse.data.previousHeadAdmin);
                     setNewHeadAdmin(notificationRetrievalResponse.data.newHeadAdmin);
                     setLoading(false);
                 } catch (err) {
@@ -33,7 +33,7 @@ function HeadAdminChangeNotification(props) {
     }, [loading, props.notificationId]);
 
 
-    const goTopreviousHeadAdminPage = () => {
+    const goToPreviousHeadAdminPage = () => {
         navigate(`/users/${previousHeadAdmin._id}`);
         props.hideModal();
     }
@@ -54,10 +54,10 @@ function HeadAdminChangeNotification(props) {
         return (
             <NotificationContentContainer>
                 {newHeadAdmin._id === userId ?
-                    <p><NavigationSpan onClick={previousHeadAdmin?.name ? goTopreviousHeadAdminPage : null}>{previousHeadAdmin?.login?.username || (previousHeadAdmin?.name?.first && previousHeadAdmin?.name?.last ? `${previousHeadAdmin.name.first} ${previousHeadAdmin.name.last}` : "deleted user")}</NavigationSpan> has made you the new head admin of group <NavigationSpan onClick={group?.name ? goToGroupPage : null}>{group.name || "deleted group"}</NavigationSpan></p>
+                    <p><NavigationSpan onClick={previousHeadAdmin?.name ? goToPreviousHeadAdminPage : null}>{previousHeadAdmin?.login?.username || (previousHeadAdmin?.name?.first && previousHeadAdmin?.name?.last ? `${previousHeadAdmin.name.first} ${previousHeadAdmin.name.last}` : "deleted user")}</NavigationSpan> has made you the new head admin of group <NavigationSpan onClick={group?.name ? goToGroupPage : null}>{group?.name || "deleted group"}</NavigationSpan></p>
                     :
                     <p>
-                        Group <NavigationSpan onClick={group?.name ? goToGroupPage : null}>{group.name || "deleted group"}</NavigationSpan> has a new head admin, <NavigationSpan onClick={newHeadAdmin?.name ? goToNewHeadAdminPage : null}>{newHeadAdmin?.login?.username || (newHeadAdmin?.name?.first && newHeadAdmin?.name?.last ? `${newHeadAdmin.name.first} ${newHeadAdmin.name.last}` : "deleted user")}</NavigationSpan>
+                        Group <NavigationSpan onClick={group?.name ? goToGroupPage : null}>{group?.name || "deleted group"}</NavigationSpan> has a new head admin, <NavigationSpan onClick={newHeadAdmin?.name ? goToNewHeadAdminPage : null}>{newHeadAdmin?.login?.username || (newHeadAdmin?.name?.first && newHeadAdmin?.name?.last ? `${newHeadAdmin.name.first} ${newHeadAdmin.name.last}` : "deleted user")}</NavigationSpan>
                     </p>
                 }
             </NotificationContentContainer>

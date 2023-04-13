c16904b64df37615496b59b7b88db352a6f384c9
Alex Rodgers <skyewulff@outlook.com>
2023-04-12 21:24:17 -0400

    tweak styling of footer; add contact link that opens form to send email to admin; create route to handle

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import useFormInput from "../hooks/useFormInput";
+import useToggle from "../hooks/useToggle";
+import { client } from "../utils";
+import Modal from "./Modal";
 
 const FooterWrapper = styled.footer`
     display: flex;
-    flex-direction: row;
-    justify-content: center;
+    flex-direction: column;
+    // justify-content: center;
     align-items: center;
     height: 100%;
-    padding: 1rem;
+    // padding: 1rem;
     background-color: black;
     opacity: 95%;
     color: #a3a3a3;
     @media (max-width: 500px) {
+        // flex-direction: column;
+        // padding-top: 1.5rem;
+    }
+`;
+
+const LinksSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    @media (max-width: 500px) {
         flex-direction: column;
-        padding-top: 1.5rem;
+        // padding-top: 1.5rem;
     }
 `;
 
@@ -43,34 +62,127 @@ const ContentWrapper = styled.p`
     }
 `;
 
+const Content = styled.span.attrs({
+    role: "button"
+})`
+
+`
+
+const PartialHr = styled.hr`
+    width: 85%;
+`;
+
+const BlockTextArea = styled.textarea`
+    display: block;
+    width: 100%;
+    margin: 1rem 0;
+`;
+
+const SubjectLabel = styled.label.attrs({
+    htmlFor: "subject"
+})`
+    margin-right: .25rem;
+    font-weight: 500;
+`;
+
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+
 function Footer() {
+    const [showContactForm, toggleShowContactForm] = useToggle(false);
+    const [subject, clearSubject, handleSubjectChange, setSubject] = useFormInput("general");
+    const [message, clearMessage, handleMessageChange, setMessage] = useFormInput();
+    const [bugScreenshot, setBugScreenshot] = useState();
+    const userId = useSelector((state) => state.login.userId);
+    
+    const handleSendEmail = async (evt) => {
+        evt.preventDefault();
+
+        if(bugScreenshot) {
+            const formData = new FormData();
+            formData.append("bug-screenshot", bugScreenshot);
+            formData.append("subject", subject === "general" ? "General" : subject === "help" ? "Help Requested" : "Bug-Screenshot");
+            formData.append("messageText", message);
+            await client.post(`${baseURL}/communications/send-email-to-site-admins`, formData, { headers: {"Content-Type": "multipart/form-data"}});
+        } else {
+            await client.post(`${baseURL}/communications/send-email-to-site-admins`, {
+                subject: subject,
+                messageText: message,
+            });
+
+        }
+        clearSubject();
+        clearMessage();
+        toggleShowContactForm();
+    }
+
+    const handleCloseContactForm = () => {
+        clearSubject();
+        clearMessage();
+        toggleShowContactForm()
+    }
+    
+    const handlebugScreenshotChange = (evt) => {
+        const file = evt.target.files[0];
+        console.log({file})
+        setBugScreenshot(file);
+    }    
+
+
+
     return (
+        <>
+        {showContactForm && 
+            <Modal hideModal={handleCloseContactForm}>
+                <form onSubmit={handleSendEmail}>
+                    <SubjectLabel>Subject:</SubjectLabel>
+                    <select id="select" name="select" value={subject} onChange={handleSubjectChange}>
+                        <option value="general">General</option>
+                        <option value="help">Help Needed</option>
+                        <option value="bug">Report an Issue</option>
+                    </select>
+                    <BlockTextArea required cols="35" rows="10" autoComplete="off" autofocus minLength={10} maxLength={5000} spellCheck={true} placeholder="Type your message here" value={message} onChange={handleMessageChange}/>
+                    {subject === "bug" && 
+                        <div>
+                            <label htmlFor="bug-screenshot">Screenshot (optional)</label>
+                            <input type="file" accept="image/*" id="bug-screenshot" name="bug-screenshot" onChange={handlebugScreenshotChange} />
+                        </div>
+                    }
+                    <br/>
+                    <button className="btn btn-success btn-md">Send</button>
+                </form>
+            </Modal>
+        }
         <FooterWrapper className="FooterWrapper">
-            <LinkBlock className="LinkBlock">
-                <LinkBlockHeading>Heading 1</LinkBlockHeading>
-                <ContentWrapper>Link 1</ContentWrapper>
-                <ContentWrapper>Link 2</ContentWrapper>
-                <ContentWrapper>Link 3</ContentWrapper>
-                <ContentWrapper>Link 4</ContentWrapper>
-                <ContentWrapper>Link 5</ContentWrapper>
-            </LinkBlock>
-            <LinkBlock className="LinkBlock">
-                <LinkBlockHeading>Heading 2</LinkBlockHeading>
-                <ContentWrapper>Link 1</ContentWrapper>
-                <ContentWrapper>Link 2</ContentWrapper>
-                <ContentWrapper>Link 3</ContentWrapper>
-                <ContentWrapper>Link 4</ContentWrapper>
-                <ContentWrapper>Link 5</ContentWrapper>
-            </LinkBlock>
-            <LinkBlock className="LinkBlock">
-                <LinkBlockHeading>Heading 3</LinkBlockHeading>
-                <ContentWrapper>Link 1</ContentWrapper>
-                <ContentWrapper>Link 2</ContentWrapper>
-                <ContentWrapper>Link 3</ContentWrapper>
-                <ContentWrapper>Link 4</ContentWrapper>
-                <ContentWrapper>Link 5</ContentWrapper>
-            </LinkBlock>
+            <LinksSection className="LinksSection">
+                <LinkBlock className="LinkBlock">
+                    <LinkBlockHeading>Heading 1</LinkBlockHeading>
+                    {userId && <ContentWrapper><Content onClick={toggleShowContactForm}>Contact</Content></ContentWrapper>}
+                    <ContentWrapper><Content>Link 2</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 3</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 4</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 5</Content></ContentWrapper>
+                </LinkBlock>
+                <LinkBlock className="LinkBlock">
+                    <LinkBlockHeading>Heading 2</LinkBlockHeading>
+                    <ContentWrapper><Content>Link 1</Content></ContentWrapper>
+                    {/* <ContentWrapper><Content>Link 2</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 3</Content></ContentWrapper> */}
+                    <ContentWrapper><Content>Link 4</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 5</Content></ContentWrapper>
+                </LinkBlock>
+                <LinkBlock className="LinkBlock">
+                    <LinkBlockHeading>Heading 3</LinkBlockHeading>
+                    <ContentWrapper><Content>Link 1</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 2</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 3</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 4</Content></ContentWrapper>
+                    <ContentWrapper><Content>Link 5</Content></ContentWrapper>
+                </LinkBlock>
+            </LinksSection>
+            <PartialHr />
+            <p>&copy; 2023 FlishFlash</p>
         </FooterWrapper>
+        </>
     )
 }
 
--- server/routes/communications.js
+++ server/routes/communications.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { getUserIdFromJWTToken } from "../utils.js";
+import { generateRandomFileName, getUserIdFromJWTToken, upload } from "../utils.js";
 import User from "../models/user.js";
+import {createTransport} from "nodemailer";
+import { getObjectSignedUrl, uploadFile } from "../s3.js";
 
 const communicationRouter = express.Router();
 
@@ -15,6 +17,51 @@ communicationRouter.get("/", getUserIdFromJWTToken, async (req, res, next) => {
         console.error(err.message);
         res.status(500).send(err.message);
     }
+});
+
+communicationRouter.post("/send-email-to-site-admins", getUserIdFromJWTToken, upload.single("bug-screenshot"), async (req, res, next) => {
+    try {
+        const sendingUser = await User.findById(req.userId, "login.email name");
+        let transporter = createTransport({
+            host: "smtp-relay.sendinblue.com",
+            port: process.env.SEND_IN_BLUE_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SEND_IN_BLUE_USER,
+                pass: process.env.SEND_IN_BLUE_PASSWORD
+            }
+        });
+
+        let bugScreenshotUrl;
+
+        if(req.file) {
+            const file = req.file;
+            const photoName = generateRandomFileName();
+            
+            await uploadFile(file.buffer, photoName, file.mimetype);
+
+            bugScreenshotUrl = await getObjectSignedUrl(photoName);
+        }
+        
+        await transporter.sendMail({
+            from: `"${sendingUser.name.first} ${sendingUser.name.last}" <${sendingUser.login.email}>`,
+            to: "admin@flishflash.org",
+            subject: `${req.body.subject}`,
+            replyTo: "no-reply@flishflash.org",
+            html: (
+                `<html>
+                    <body>
+                        <p>${req.body.messageText}</p>
+                        ${bugScreenshotUrl ? `<img src=${bugScreenshotUrl} alt="bug-screenshot" />` : ""}
+                    </body>
+                </html>`
+            )
+        });
+        res.status(200).send("Message sent successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("There was an error sending your message. YOu can try again or if you like you can email us directly at admin@flishflash.org")
+    } 
 });
 
 export default communicationRouter;

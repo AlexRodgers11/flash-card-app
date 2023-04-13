ea7390560421d4ed92a6a20ca14d8556a49ea3bc
Alex Rodgers <skyewulff@outlook.com>
2023-04-13 16:53:53 -0400

    add error handling to RegisterIdentificationForm for if a user tries to claim a username that is already taken

--- client/src/components/RegisterIdentificationForm.js
+++ client/src/components/RegisterIdentificationForm.js
@@ -1,8 +1,19 @@
+import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import useFormInput from '../hooks/useFormInput';
 import { updateProfilePic, updateUser } from '../reducers/loginSlice';
+import styled from 'styled-components';
+
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+
+const ErrorAlert = styled.div`
+    margin: .25rem 0;
+    border-radius: .25rem;
+    background-color: #f55656;
+    color: white;
+`;
 
 function RegisterIdentificationForm() {
     const dispatch = useDispatch();
@@ -11,31 +22,41 @@ function RegisterIdentificationForm() {
     const [firstName, clearFirstName, setFirstName] = useFormInput('');
     const [lastName, clearLastName, setLastName] = useFormInput('');
     const [photo, setPhoto] = useState();
+    const [errorMessage, setErrorMessage] = useState("");
     const userId = useSelector((state) => state.login.userId);
     const name = useSelector((state) => state.login.name);
     const email = useSelector((state) => state.login.login.email);
     const storedPhoto = useSelector((state) => state.login.photo);
 
-    const handleSubmit = evt => {
+    const handleSubmit = async (evt) => {
+        setErrorMessage("");
         evt.preventDefault();
-        if(photo) {
-            dispatch(updateProfilePic({userId, photo}));
-        }
-        dispatch(updateUser({userId, userUpdates: {login: {username: username, email: email}, name: {first: firstName, last: lastName}}}))
-            .then(() => {
-                if(photo) {
-                    console.log("navigating to profile-pic-crop")
-                    navigate("/register/profile-pic-crop");
+        let usernameResponse = await axios.get(`${baseURL}/login/usernames?username=${username}`);
+        if(usernameResponse.data.usernameAvailable) {
+            if(photo) {
+                dispatch(updateProfilePic({userId, photo}));
+            }
+            dispatch(updateUser({userId, userUpdates: {login: {username: username, email: email}, name: {first: firstName, last: lastName}}}))
+                .then(() => {
+                    if(photo) {
+                        console.log("navigating to profile-pic-crop")
+                        navigate("/register/profile-pic-crop");
 
-                } else {
-                    console.log("navigating to join groups")
-                    navigate("/register/join-groups");
-                }
-            })
-        clearUsername();
-        clearFirstName();
-        clearLastName();
-        setPhoto("");
+                    } else {
+                        console.log("navigating to join groups")
+                        navigate("/register/join-groups");
+                    }
+                })
+                clearUsername();
+                clearFirstName();
+                clearLastName();
+                setPhoto("");
+                // setEditField("");
+            } else {
+                // setErrorField("username");
+                setErrorMessage("This username is taken");
+            }
+        
     }
 
     const handlePhotoChange = (evt) => {
@@ -70,6 +91,11 @@ function RegisterIdentificationForm() {
                     <label htmlFor="username">Username (optional)</label>
                     <input type="text" id="username" name="username" value={username} onChange={setUsername} />
                 </div>
+                {(errorMessage) && 
+                    <ErrorAlert role="alert">
+                        {errorMessage}
+                    </ErrorAlert>
+                }
                 <div>
                     <label htmlFor="firstName">First Name</label>
                     <input type="text" id="firstName" name="firstName" value={firstName} onChange={setFirstName} />

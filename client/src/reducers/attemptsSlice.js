4d5432755fea32ee5008fe743a356626073cf3af
Alex Rodgers <skyewulff@outlook.com>
2023-04-22 14:18:35 -0400

    convert dates to local dates/times for all statistics-based requests in attemptsSlice

--- client/src/reducers/attemptsSlice.js
+++ client/src/reducers/attemptsSlice.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 import { client } from "../utils";
 
 const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
@@ -18,13 +17,28 @@ const initialState = {
 
 export const fetchUserAttempts = createAsyncThunk("attempts/fetchUserAttempts", async ({userId}) => {
     const response = await client.get(`${baseURL}/users/${userId}/attempts`);
-    return response.data;
+    const dateConvertedAttempts = response.data.map(attempt => {
+        const date = new Date(attempt.datePracticed);
+        const options = {month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+        const locale = navigator.language;
+        const localDateString = date.toLocaleDateString(locale || "en-US", options);
+        return {...attempt, datePracticed: localDateString};
+    });
+    
+    return dateConvertedAttempts;
 });
 
 export const fetchDeckAttempts = createAsyncThunk("attempts/fetchDeckAttempts", async ({deckId}) => {
     const response = await client.get(`${baseURL}/decks/${deckId}/attempts`);
+    const dateConvertedAttempts = response.data.map(attempt => {
+        const date = new Date(attempt.datePracticed);
+        const options = {month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+        const locale = navigator.language;
+        const localDateString = date.toLocaleDateString(locale || "en-US", options);
+        return {...attempt, datePracticed: localDateString};
+    });
     return {
-        attempts: response.data,
+        attempts: dateConvertedAttempts,
         deckId: deckId
     }
 });
@@ -32,13 +46,35 @@ export const fetchDeckAttempts = createAsyncThunk("attempts/fetchDeckAttempts", 
 
 export const fetchCardStatsByDeck = createAsyncThunk("attempts/fetchStatsCardIds", async ({userId}) => {
     const response = await client.get(`${baseURL}/users/${userId}/card-stats`);
-    return response.data;
+    const dateConvertedStats = response.data.map(deck => {
+        const dateConvertedCardStats = deck.cards.map(card => {
+            if(card.dateLastPracticed) {
+                const date = new Date(card.dateLastPracticed);
+                const options = {month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+                const locale = navigator.language;
+                const localDateString = date.toLocaleDateString(locale || "en-US", options);
+                return {...card, dateLastPracticed: localDateString};
+            } else {
+                return card;
+            }
+        });
+        return {...deck, cards: dateConvertedCardStats};
+    });
+
+    return dateConvertedStats;
 });
 
 export const fetchCardAttempts = createAsyncThunk("attempts/fetchCardAttempts", async ({cardId}) => {
     const response = await client.get(`${baseURL}/cards/${cardId}/attempts`);
+    const dateConvertedAttempts = response.data.attempts.map(attempt => {
+        const date = new Date(attempt.datePracticed);
+        const options = {month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+        const locale = navigator.language;
+        const localDateString = date.toLocaleDateString(locale || "en-US", options);
+        return {...attempt, datePracticed: localDateString};
+    })
     return {
-        attempts: response.data.attempts,
+        attempts: dateConvertedAttempts,
         selectedCard: response.data.selectedCard
     }
 });
@@ -50,7 +86,19 @@ export const fetchDeckAttemptData = createAsyncThunk("attempts/fetchDeckAttemptD
 
 export const fetchAllDecksStats = createAsyncThunk("attempts/fetchAllDecksStats", async ({userId}) => {
     const response = await client.get(`${baseURL}/users/${userId}/decks/statistics`);
-    return response.data;
+    const dateConvertedStats = response.data.map(deck => {
+        if(deck.dateLastPracticed) {
+            const date = new Date(deck.dateLastPracticed);
+            const options = {month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+            const locale = navigator.language;
+            const localDateString = date.toLocaleDateString(locale || "en-US", options);
+            return {...deck, dateLastPracticed: localDateString};
+        } else {
+            return deck;
+        }
+    });
+
+    return dateConvertedStats;
 });
 
 export const attemptsSlice = createSlice({
@@ -66,16 +114,7 @@ export const attemptsSlice = createSlice({
         builder.addCase(fetchCardAttempts.fulfilled, (state, action)=> {
             state.cardAttempts = action.payload.attempts;
             state.selectedCard = action.payload.selectedCard;
-            // state.selectedCardId = action.payload.cardId;
-            // state.selectedCardQuestion = action.payload.cardQuestion;
-            // state.selectedCardAnswer = action.payload.cardAnswer;
         });
-        // builder.addCase(fetchCardAttemptIds.fulfilled, (state, action)=> {
-        //     state.cardAttemptIds = action.payload.attempts;
-        //     state.selectedCardId = action.payload.cardId;
-        //     state.selectedCardQuestion = action.payload.cardQuestion;
-        //     state.selectedCardAnswer = action.payload.cardAnswer;
-        // });
         builder.addCase(fetchDeckAttempts.fulfilled, (state, action) => {
             state.deckAttempts = action.payload.attempts;
             state.selectedDeckId = action.payload.deckId;
@@ -89,9 +128,6 @@ export const attemptsSlice = createSlice({
         builder.addCase(fetchAllDecksStats.fulfilled, (state, action) => {
             state.decksStats = action.payload;
         });
-        // builder.addCase(fetchStatsDeckIds.fulfilled, (state, action) => {
-        //     state.deckIds = action.payload;
-        // });
     }
 });
 

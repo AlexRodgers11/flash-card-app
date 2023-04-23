77ab12f12092d5694a2a4dce2d558068506a5ddd
Alex Rodgers <skyewulff@outlook.com>
2023-04-23 00:49:19 -0400

    add privacy and communicationSettings properties from loginSlice to persistor whitelist

--- client/src/store.js
+++ client/src/store.js
@@ -12,7 +12,7 @@ import storage from "redux-persist/lib/storage";
 const loginPersistConfig = {
     key: "login",
     storage: storage,
-    whitelist: ["accountSetupStage", "login.username", "photo", "name", "userId"]
+    whitelist: ["accountSetupStage", "login.username", "photo", "name", "userId", "privacy", "communicationSettings"]
 }
 
 const persistedLoginReducer = persistReducer(loginPersistConfig, loginReducer);
